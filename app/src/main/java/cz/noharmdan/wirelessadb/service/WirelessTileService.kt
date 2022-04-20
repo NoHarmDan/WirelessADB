6bc18071aef38904bd6e433bea768a6564cd8eab
Daniel Pichl <pichl@mobilesoft.eu>
2022-04-20 15:24:01 +0200

    Coroutine cancelation

--- app/src/main/java/cz/noharmdan/wirelessadb/service/WirelessTileService.kt
+++ app/src/main/java/cz/noharmdan/wirelessadb/service/WirelessTileService.kt
@@ -6,17 +6,13 @@ import android.service.quicksettings.TileService
 import androidx.annotation.RequiresApi
 import cz.noharmdan.wirelessadb.WADBApplication
 import cz.noharmdan.wirelessadb.repository.GlobalSettingsRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 @RequiresApi(Build.VERSION_CODES.N)
 class WirelessTileService : TileService() {
 
     private val uiJob = SupervisorJob()
     private val uiScope = CoroutineScope(Dispatchers.Main + uiJob + WADBApplication.coroutineExceptionHandler)
-
 
     override fun onStartListening() {
         super.onStartListening()
@@ -26,6 +22,11 @@ class WirelessTileService : TileService() {
                 updateTileState(it)
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        uiScope.cancel()
     }
 
     override fun onClick() {
